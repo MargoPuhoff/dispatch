c119bb5b09eff0997217c5f5077903206289df3f
Пухова Маргарита Сергеевна <margo@puhoff.com>
2024-05-20 00:05:08 +0300

    Добавила annotate

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: drivers
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  email      :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Driver < ApplicationRecord
 end
