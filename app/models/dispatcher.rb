206be3acd539f9af183ed63304d96ed3ff1aee8c
Пухова Маргарита Сергеевна <margo@puhoff.com>
2024-05-24 15:07:05 +0300

    Настроила devise

--- app/models/dispatcher.rb
+++ app/models/dispatcher.rb
@@ -12,6 +12,15 @@
 #  password_digest :string
 #
 class Dispatcher < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable,
+         :confirmable, 
+         # :rememberable, 
+         :validatable
+         
   has_many :treks
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :dispatchers
   resources :dispatchers
   resources :treks do
     member do
