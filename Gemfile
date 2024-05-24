9af7ac294e8ce73eb7a7d7ca5d09390c3916e4b7
Пухова Маргарита Сергеевна <margo@puhoff.com>
2024-05-24 13:32:05 +0300

    Добавила гем devise

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '3.2.3'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'bcrypt'
+gem 'devise'
 gem 'faker'
 gem 'html2slim'
 gem 'rails', '~> 7.1.3', '>= 7.1.3.2'
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,13 @@ Rails.application.routes.draw do
   resources :autos
   resources :drivers
 
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  get  'login'   			 => 'sessions#new'
+  post 'login'   			 => 'sessions#create'
+  get  'logout'  			 => 'sessions#destroy'
 
-  get 'driver_login' => 'driver_sessions#new'
-  post 'driver_login' => 'driver_sessions#create'
-  get 'driver_logout' => 'driver_sessions#destroy'
+  get  'driver_login'  => 'driver_sessions#new'
+  post 'driver_login'  => 'driver_sessions#create'
+  get  'driver_logout' => 'driver_sessions#destroy'
 
-  # Defines the root path route ("/")
   root 'home#index'
 end
